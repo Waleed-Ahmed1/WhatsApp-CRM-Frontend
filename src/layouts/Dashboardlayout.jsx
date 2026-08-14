4692c658a335f5c00f8176a2cdd8f5097103890c
Muhammad-Raheel04 <raheel31102004@gmail.com>
2026-08-14 09:21:21 +0500

    feat: update ui

--- src/layouts/Dashboardlayout.jsx
+++ src/layouts/Dashboardlayout.jsx
@@ -5,44 +5,45 @@ import { logoutuser } from "../api/auth";
 import toast from "react-hot-toast";
 
 export default function DashboardLayout() {
+  const navigate = useNavigate();
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const navigate = useNavigate()
-    const [isExpanded, setIsExpanded] = useState(false);
+  const local_store = JSON.parse(localStorage.getItem("user"));
+  const user_name = local_store?.name || "unknown";
+  const email_user = local_store?.email || "unknown@gmail.com";
 
-    const local_store = JSON.parse(localStorage.getItem("user"))
-    const user_name = local_store?.name || "unknown";
-    const email_user = local_store?.email || "unknown@gmail.com";
-
-    const logout = async () => {
-        try {
-            const res = await logoutuser();
-            localStorage.removeItem("token");
-            localStorage.removeItem("user")
-            toast.success(res.data.message || "Logged out Successfully.")
-            navigate('/login')
-        } catch (err) {
-            console.error(err)
-            toast.error(err.response?.data?.message || "Logout Failed! Try Again later.")
-        }
+  const logout = async () => {
+    try {
+      const res = await logoutuser();
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      toast.success(res.data.message || "Logged out Successfully.");
+      navigate("/login");
+    } catch (err) {
+      console.error(err);
+      toast.error(
+        err.response?.data?.message || "Logout Failed! Try Again later.",
+      );
     }
+  };
 
-    return (
-        <div className="h-screen w-full overflow-hidden bg-[#EAF7F4]">
-            <Sidebar
-                isExpanded={isExpanded}
-                setIsExpanded={setIsExpanded}
-                onLogout={logout}
-                username={user_name}
-                email={email_user}
-            />
+  return (
+    <div className="flex h-dvh w-full flex-col overflow-hidden bg-[#EAF7F4]">
+      <Sidebar
+        isExpanded={isExpanded}
+        setIsExpanded={setIsExpanded}
+        onLogout={logout}
+        username={user_name}
+        email={email_user}
+      />
 
-            <main
-                className={`h-screen overflow-hidden transition-all duration-300 ${
-                    isExpanded ? "lg:ml-60" : "lg:ml-[55px]"
-                }`}
-            >
-                <Outlet />
-            </main>
-        </div>
-    );
+      <main
+        className={`min-h-0 flex-1 overflow-hidden transition-all duration-300 ${
+          isExpanded ? "lg:ml-60" : "lg:ml-[55px]"
+        }`}
+      >
+        <Outlet />
+      </main>
+    </div>
+  );
 }
